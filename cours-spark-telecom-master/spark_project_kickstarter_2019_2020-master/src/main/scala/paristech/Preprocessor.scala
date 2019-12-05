1d0022d93b88d0e6214928c6a7f971871df8630a
Thomas <thomas.koch.ea2011@gmail.com>
2019-12-05 19:36:13 +0100

    Finalisation des codes du projet

--- cours-spark-telecom-master/spark_project_kickstarter_2019_2020-master/src/main/scala/paristech/Preprocessor.scala
+++ cours-spark-telecom-master/spark_project_kickstarter_2019_2020-master/src/main/scala/paristech/Preprocessor.scala
@@ -43,14 +43,26 @@ object Preprocessor {
       *       if problems with unimported modules => sbt plugins update
       *
       ********************************************************************************/
+
+
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Chargement des donnees                                       //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+
+
     val df: DataFrame = spark
       .read
-      .option("header", true) // utilise la première ligne du (des) fichier(s) comme header
+      .option("header", value = true) // utilise la première ligne du (des) fichier(s) comme header
       .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
-      .csv("/home/p5hngk/Downloads/GitHub/INF_729---Introduction_au_framework_Hadoop/cours-spark-telecom-master/data/train_clean.csv")
+      .csv("data/train_clean.csv")
+
+    println(df.printSchema())
+    println(df.show(5))
 
 
-    df.printSchema()
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Assignation des types Int aux colonnes adequates             //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
 
     val dfCasted: DataFrame = df
       .withColumn("goal", $"goal".cast("Int"))
@@ -61,17 +73,26 @@ object Preprocessor {
       .withColumn("backers_count", $"backers_count".cast("Int"))
       .withColumn("final_status", $"final_status".cast("Int"))
 
-    dfCasted.printSchema()
+    println(dfCasted.printSchema())
+    println(dfCasted.show(5))
 
-    dfCasted
-      .select("goal", "backers_count", "final_status")
-      .describe()
-      .show
+
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Cleaning fuites du futur et disable_communication            //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
 
     val dfNoFutur: DataFrame = dfCasted
       .drop("disable_communication")
       .drop("backers_count", "state_changed_at")
 
+    println(dfNoFutur.printSchema())
+    println(dfNoFutur.show(5))
+
+
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Cleaning colonnes currency, country et final_status          //")
+    println("//                    Creation des colonnes days_campaign et hours_prepa           //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
 
     def cleanCountry(country: String, currency: String): String = {
       if (country == "False")
@@ -96,11 +117,17 @@ object Preprocessor {
       .drop("country", "currency")
       .filter($"final_status" === 0 || $"final_status" === 1)
       .withColumn("days_campaign", datediff(from_unixtime($"deadline") , from_unixtime($"launched_at")))
-      .withColumn("hours_prepa", ((($"launched_at" - $"created_at")/3.6).cast("Int")/1000))
+      .withColumn("hours_prepa", (($"launched_at" - $"created_at")/3.6).cast("Int")/1000)
+
+    println(dfCountry.printSchema())
+    println(dfCountry.show(5))
 
 
-    dfCountry.printSchema
-
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Cleaning colonnes name, desc, keywords, hours prepa,         //")
+    println("//                      created_at, deadline et launched_at                        //")
+    println("//                    Cleaning des valeurs nulles                                  //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
 
     def cleanHoursPrepa(created_at: Int, launched_at: Int, hours_prepa: Int): Int = {
       if (hours_prepa < 0)
@@ -113,25 +140,29 @@ object Preprocessor {
 
     val dfCountry2: DataFrame = dfCountry
       .withColumn("created_at2", cleanHoursPrepaUdf($"created_at", $"launched_at", $"hours_prepa"))
-      .withColumn("hours_prepa", ((($"launched_at" - $"created_at2")/3.6).cast("Int")/1000))
+      .withColumn("hours_prepa", (($"launched_at" - $"created_at2")/3.6).cast("Int")/1000)
       .drop("created_at")
       .drop("launched_at", "created_at2", "deadline")
       .withColumn("name", lower($"name"))
       .withColumn("desc", lower($"desc"))
       .withColumn("keywords", lower($"keywords"))
       .withColumn("text", concat_ws(" ", $"name", $"desc", $"keywords"))
-      .withColumn("days_campaign", when($"days_campaign".isNull, -1).otherwise("days_campaign"))
-      .withColumn("hours_prepa", when($"hours_prepa".isNull, -1).otherwise("hours_prepa"))
-      .withColumn("goal", when($"goal".isNull, -1).otherwise("goal"))
-      .withColumn("country2", when($"country2".isNull, "unknown").otherwise("country2"))
-      .withColumn("currency2", when($"currency2".isNull, "unknown").otherwise("currency2"))
+      .withColumn("days_campaign", when($"days_campaign".isNull, -1).otherwise("$days_campaign"))
+      .withColumn("hours_prepa", when($"hours_prepa".isNull, -1).otherwise("$hours_prepa"))
+      .withColumn("goal", when($"goal".isNull, -1).otherwise("$goal"))
+      .withColumn("country2", when($"country2".isNull, "unknown").otherwise("$country2"))
+      .withColumn("currency2", when($"currency2".isNull, "unknown").otherwise("$currency2"))
+
+    println(dfCountry2.printSchema())
+    println(dfCountry2.show(5))
+
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+    println("//                    Sauvegarde du DataFrame => monDataFrameFinal                 //")
+    println("/////////////////////////////////////////////////////////////////////////////////////")
+
 
     val monDataFrameFinal: DataFrame = dfCountry2
 
-    monDataFrameFinal.write.mode("overwrite").parquet("/home/p5hngk/Downloads/GitHub/INF_729---Introduction_au_framework_Hadoop/cours-spark-telecom-master/monDataFrameFinal")
-
-    println("\n")
-    println("Hello World ! from Preprocessor")
-    println("\n")
+    monDataFrameFinal.write.mode("overwrite").parquet("monDataFrameFinal")
   }
 }
